3686c5663d00ce5534bf62ab86956b9d5fc4a27a
lkIcom2000 <47300409+lkIcom2000@users.noreply.github.com>
2025-04-27 21:09:58 +0200

    finalize userService

--- user-service/user-service/src/main/java/dk/au/userservice/UserServiceApplication.java
+++ user-service/user-service/src/main/java/dk/au/userservice/UserServiceApplication.java
@@ -3,9 +3,11 @@ package dk.au.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "dk.au.userservice")
 @EntityScan(basePackages = "dk.au.userservice.model")
 @EnableJpaRepositories(basePackages = "dk.au.userservice.repo")
 public class UserServiceApplication {
